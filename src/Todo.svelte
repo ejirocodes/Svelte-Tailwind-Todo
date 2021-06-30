047ba78c49968603c61685fc670d0fee74f61f0a
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-06-30 15:27:16 +0100

    update todo

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,27 +1,26 @@
 <script>
+  import { onMount } from 'svelte';
+
   let todos = [
     {
       title: 'Todo A',
-      project: 'Project A',
       done: false,
     },
     {
       title: 'Todo B',
-      project: 'Project B',
       done: true,
     },
     {
       title: 'Todo C',
-      project: 'Project C',
       done: false,
     },
     {
       title: 'Todo D',
-      project: 'Project D',
       done: false,
     },
   ];
   let todoItem = '';
+
   const addTodo = () => {
     // Using push to trigger update
     // todos.push({
@@ -57,7 +56,11 @@
   const toggleComplete = (todo) => {
     const todoIndex = todos.indexOf(todo);
     todos[todoIndex].done = !todos[todoIndex].done;
-    console.log(todos);
+  };
+
+  const updateTodo = (todo) => {
+    console.log({ todo });
+    console.log({ todos });
   };
 </script>
 
@@ -66,6 +69,27 @@
   <p>Today's tasks</p>
   <input type="text" bind:value={todoItem} />
   <button on:click={addTodo}>Add</button>
+
+  <ul>
+    {#each todos as todo}
+      <li class="todo-item-wrapper">
+        <input
+          type="checkbox"
+          bind:checked={todo.done}
+          on:click={toggleComplete(todo)}
+        />
+        <input
+          class:completed={todo.done}
+          class="border-0"
+          bind:value={todo.title}
+          on:change={updateTodo(todo)}
+        />
+        <span on:click={deleteTodo(todo)}>X</span>
+      </li>
+    {:else}
+      <p>No item in to-do!</p>
+    {/each}
+  </ul>
   <p>
     Completed Tasks: {todos.filter((todo) => {
       return todo.done;
@@ -76,21 +100,6 @@
       return todo.done === false;
     }).length}
   </p>
-  <ul>
-    {#each todos as todo}
-      <li class="todo-item" on:click={toggleComplete(todo)}>
-        <input
-          type="checkbox"
-          class="form-checkbox text-indigo-600"
-          bind:checked={todo.done}
-        />
-        <span class:completed={todo.done}>{todo.title} </span>
-        <span on:click={deleteTodo(todo)}>X</span>
-      </li>
-    {:else}
-      <p>No item in to-do!</p>
-    {/each}
-  </ul>
 </main>
 
 <style>
@@ -108,19 +117,6 @@
   .completed {
     text-decoration: line-through;
   }
-  @keyframes strike {
-    from {
-      text-decoration-color: transparent;
-    }
-    to {
-      text-decoration-color: auto;
-    }
-  }
   .todo-item {
-    /* transition: all cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.2s; */
-  }
-  .todo-item:hover {
-    text-decoration: line-through;
-    animation: strike 0.2s linear;
   }
 </style>
